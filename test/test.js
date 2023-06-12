0d72948d7b6cd3ab1dfa89db7a2dd7c2734e45c8
sravani <sravani111.ce@gmail.com>
2023-06-12 16:00:07 +0530

    nodejs2

--- test/test.js
+++ test/test.js
@@ -1,7 +1,18 @@
-var request = require('supertest');
-var app = require('../index.js');
+const axios = require('axios');
+const app = require('../index.js');
+const { expect } = require('chai');
+
 describe('GET /will', function() {
-    it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
-    });
+  it('respond with hello world', async function() {
+    try {
+      const response = await axios.get('http://localhost:4001/will');
+      const expectedResponse = { response: 'Hello World' };
+
+      // Assert the response data matches the expected response
+      expect(response.data).to.deep.equal(expectedResponse);
+    } catch (error) {
+      // Handle any errors that occur during the request
+      throw error;
+    }
+  });
 });
